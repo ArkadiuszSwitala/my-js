9b26a1371d055986b3ec26b5ea50b143171729b6
PanBec <panbec@wp.pl>
2019-09-11 20:35:37 +0200

    Refactor

--- js/binding.js
+++ js/binding.js
@@ -1,99 +1,67 @@
 function Data(dataObj) {
-    this.dataObject = dataObj
+    this.dataObject = {}
     this.dataElements = {}
     this.classElements = {}
-
-    let allElements = document.querySelectorAll('*')
-
-    allElements.forEach(element => {
-
-        this.getElementsByAttribute(element, 'data-bind', this.dataElements);
-        this.getElementsByAttribute(element, 'class-bind', this.classElements);
-
-        // if (element.hasAttribute('data-bind')) {
-        //     let attValue = element.getAttribute('data-bind')
-        //     let keyValueArr = attValue.split(':').map(item => item.trim())
-        //     if (!dataObj.hasOwnProperty(keyValueArr[1])) {
-        //         return
-        //     }
-        //
-        //     if (!dataElements.hasOwnProperty(keyValueArr[1])) {
-        //         dataElements[keyValueArr[1]] = []
-        //     }
-        //     dataElements[keyValueArr[1]].push({
-        //         element: element,
-        //         property: keyValueArr[0]
-        //     })
-        // }
-        //
-        // if (element.hasAttribute('class-bind')) {
-        //     let attValue = element.getAttribute('class-bind')
-        //     let keyValueArr = attValue.split(':').map(item => item.trim())
-        //     if (!dataObj.hasOwnProperty(keyValueArr[1])) {
-        //         return
-        //     }
-        //     if (!this.classElements.hasOwnProperty(keyValueArr[1])) {
-        //         classElements[keyValueArr[1]] = []
-        //     }
-        //     classElements[keyValueArr[1]].push({
-        //         element: element,
-        //         className: keyValueArr[0]
-        //     })
-        // }
-    });
-
+    this.prepareData = function (){
+        for(let prop in dataObj){
+            this.dataObject[`_${prop}`] = dataObj[prop]
+        }
+    }
     this.getElementsByAttribute = function (element, attribute, targetObject) {
         if (element.hasAttribute(attribute)) {
             let keyValueArr = element.getAttribute(attribute).split(':').map(item => item.trim())
-            if (!dataObj.hasOwnProperty(keyValueArr[1])) {
+            if (!this.dataObject.hasOwnProperty(`_${keyValueArr[1]}`)) {
                 return
             }
 
-            if (!targetObject.hasOwnProperty(keyValueArr[1])) {
-                targetObject[keyValueArr[1]] = []
+            if (!targetObject.hasOwnProperty(`_${keyValueArr[1]}`)) {
+                targetObject[`_${keyValueArr[1]}`] = []
             }
-            targetObject[keyValueArr[1]].push({
+
+            targetObject[`_${keyValueArr[1]}`].push({
                 element: element,
                 property: keyValueArr[0]
             })
         }
     }
-
-    console.log("Data elements: ", dataElements)
-    console.log("Class elements: ", classElements)
-
-    for (let key in dataObj) {
-        if (dataObj.hasOwnProperty(key)) {
-            if (dataElements.hasOwnProperty(key)) {
-                for (let item in dataElements[key]) {
-                    dataElements[key][item].element[dataElements[key][item].property] = dataObj[key]
-                }
+    this.updateDom = function (key){
+        if (this.dataElements.hasOwnProperty(key)) {
+            for (let item in this.dataElements[key]) {
+                this.dataElements[key][item].element[this.dataElements[key][item].property] = this.dataObject[key]
             }
-            if (classElements.hasOwnProperty(key)) {
-                for (let item in classElements[key]) {
-                    let el = classElements[key][item]
-                    el.element.classList.toggle(el.className, dataObj[key])
-                }
+        }
+        if (this.classElements.hasOwnProperty(key)) {
+            for (let item in this.classElements[key]) {
+                let el = this.classElements[key][item]
+                el.element.classList.toggle(el.property, this.dataObject[key])
             }
-            Object.defineProperty(dataObj, `_${key}`, {
-                set: function (v) {
-                    dataObj[key] = v
-                    if (dataElements.hasOwnProperty(key)) {
-                        for (let item in dataElements[key]) {
-                            dataElements[key][item].element[dataElements[key][item].property] = dataObj[key]
-                        }
-                    }
+        }
+    }
 
-                    if (classElements.hasOwnProperty(key)) {
-                        for (let item in classElements[key]) {
-                            let el = classElements[key][item]
-                            el.element.classList.toggle(el.className, dataObj[key])
-                        }
-                    }
+    this.prepareData()
+
+    let allElements = document.querySelectorAll('*')
+
+    allElements.forEach(element => {
+        this.getElementsByAttribute(element, 'data-bind', this.dataElements);
+        this.getElementsByAttribute(element, 'class-bind', this.classElements);
+    });
+
+    for (let key in this.dataObject) {
+        if (this.dataObject.hasOwnProperty(key)) {
+ 
+        this.updateDom(key)
+            Object.defineProperty(this.dataObject, key.substr(1), {
+                set: (v) => {
+                    this.dataObject[key] = v
+                    this.updateDom(key)
+                },
+                get: () => {
+                    return this.dataObject[key]
                 }
             });
         }
     }
-
-    return dataObj
+    
+    return this.dataObject
 }
